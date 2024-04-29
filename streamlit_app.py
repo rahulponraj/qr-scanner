5afdb2d82fb456d08ddd0af2e161b85aa2bbd6a9
rahulponraj <rahulponraj@gmail.com>
2024-04-29 17:19:53 +0530

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,55 +1,43 @@
 import streamlit as st
-import pandas as pd
-from io import StringIO
-import datetime
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, ClientSettings
+import cv2
+from pyzbar.pyzbar import decode
 
- 
+# Initialize an empty list to store scanned data
+scanned_data = []
 
+st.title("Mobile QR Code Scanner")
 
+webrtc_ctx = webrtc_streamer(
+    key="example",
+    video_transformer_factory=None,  # No need for video transformation
+    client_settings=ClientSettings(
+        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+    ),
+    mode=WebRtcMode.SENDRECV,
+    async_transform=True
+)
 
+if not webrtc_ctx.video_receiver:
+    st.warning("Please allow access to the camera.")
+    st.stop()
 
-# df = pd.read_csv("second.csv")
-# st.line_chart(df)
+video_frame = webrtc_ctx.video_receiver.get_frame()
 
-# Using columns to create a two-column layout
-col1, col2 = st.columns(2)
-with col1:
-    st.write("Column 1")
-    st.markdown("# <span style='color: blue;'>Hello *world!* </span>", unsafe_allow_html=True)
-    uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+# Use video_frame.data for QR code detection and decoding
+# Here, we'll detect QR codes in each frame and add the data to the scanned_data list
+if video_frame:
+    frame = video_frame.to_ndarray()
+    # Convert the frame to grayscale for QR code detection
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Detect QR codes in the frame
+    qrcodes = decode(gray_frame)
+    # Extract data from detected QR codes
+    for qrcode in qrcodes:
+        data = qrcode.data.decode("utf-8")
+        if data not in scanned_data:
+            scanned_data.append(data)
 
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
-    
-    
-with col2:
-    st.write("Column 2")
-    age = st.slider('How old are you?', 0, 130, 25)
-    st.write("I'm ", age, 'years old')
-d = st.date_input("When's your birthday", datetime.date(2019, 7, 6))
-st.write('Your birthday is:', d)
-# Using sidebar for a side panel
-with st.sidebar:
-    st.write("Welcome")
-    st.write("Home")
-    st.write("FAQ")
-    st.write("Support")
-
-# Using expander to collapse/expand content
-with st.expander("Click to expand"):
-    st.write("This is for test")
-    st.write("Hey")
-    st.write("Hi There!")
+# Display the table of scanned data
+st.write("Scanned Data:")
+st.table(scanned_data)
